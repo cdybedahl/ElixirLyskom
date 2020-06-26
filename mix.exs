e3a04ac3a12666de6876fad23795c2477d87d19a
Calle Dybedahl <github@cyberpomo.com>
2020-06-26 17:43:13 +0200

    Bump version number.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lyskom.MixProject do
   def project do
     [
       app: :lyskom,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
