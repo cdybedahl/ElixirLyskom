bba2db94864c0683caa678484bfb1125bc4ab31f
Calle Dybedahl <github@cyberpomo.com>
2020-07-01 11:55:44 +0200

    Use macros instead of hand-coded lists of identical functions.

--- lib/lyskom/constants.ex
+++ lib/lyskom/constants.ex
@@ -1,168 +1,109 @@
 defmodule Lyskom.Constants do
-  def async(0), do: :async_new_text_old
-  def async(1), do: :async_i_am_off
-  def async(2), do: :async_i_am_on_obsolete
-  def async(5), do: :async_new_name
-  def async(6), do: :async_i_am_on
-  def async(7), do: :async_sync_db
-  def async(8), do: :async_leave_conf
-  def async(9), do: :async_login
-  def async(10), do: :async_broadcast
-  def async(11), do: :async_rejected_connection
-  def async(12), do: :async_send_message
-  def async(13), do: :async_logout
-  def async(14), do: :async_deleted_text
-  def async(15), do: :async_new_text
-  def async(16), do: :async_new_recipient
-  def async(17), do: :async_sub_recipient
-  def async(18), do: :async_new_membership
-  def async(19), do: :async_new_user_area
-  def async(20), do: :async_new_presentation
-  def async(21), do: :async_new_motd
-  def async(22), do: :async_text_aux_changed
-  def async('0'), do: :async_new_text_old
-  def async('1'), do: :async_i_am_off
-  def async('2'), do: :async_i_am_on_obsolete
-  def async('5'), do: :async_new_name
-  def async('6'), do: :async_i_am_on
-  def async('7'), do: :async_sync_db
-  def async('8'), do: :async_leave_conf
-  def async('9'), do: :async_login
-  def async('10'), do: :async_broadcast
-  def async('11'), do: :async_rejected_connection
-  def async('12'), do: :async_send_message
-  def async('13'), do: :async_logout
-  def async('14'), do: :async_deleted_text
-  def async('15'), do: :async_new_text
-  def async('16'), do: :async_new_recipient
-  def async('17'), do: :async_sub_recipient
-  def async('18'), do: :async_new_membership
-  def async('19'), do: :async_new_user_area
-  def async('20'), do: :async_new_presentation
-  def async('21'), do: :async_new_motd
-  def async('22'), do: :async_text_aux_changed
+  @asyncs %{
+    0 => :async_new_text_old,
+    1 => :async_i_am_off,
+    2 => :async_i_am_on_obsolete,
+    5 => :async_new_name,
+    6 => :async_i_am_on,
+    7 => :async_sync_db,
+    8 => :async_leave_conf,
+    9 => :async_login,
+    10 => :async_broadcast,
+    11 => :async_rejected_connection,
+    12 => :async_send_message,
+    13 => :async_logout,
+    14 => :async_deleted_text,
+    15 => :async_new_text,
+    16 => :async_new_recipient,
+    17 => :async_sub_recipient,
+    18 => :async_new_membership,
+    19 => :async_new_user_area,
+    20 => :async_new_presentation,
+    21 => :async_new_motd,
+    22 => :async_text_aux_changed
+  }
 
-  def error_code(0), do: :no_error
-  def error_code(2), do: :not_implemented
-  def error_code(3), do: :obsolete_call
-  def error_code(4), do: :invalid_password
-  def error_code(5), do: :string_too_long
-  def error_code(6), do: :login_first
-  def error_code(7), do: :login_disallowed
-  def error_code(8), do: :conference_zero
-  def error_code(9), do: :undefined_conference
-  def error_code(10), do: :undefined_person
-  def error_code(11), do: :access_denied
-  def error_code(12), do: :permission_denied
-  def error_code(13), do: :not_member
-  def error_code(14), do: :no_such_text
-  def error_code(15), do: :text_zero
-  def error_code(16), do: :no_such_local_text
-  def error_code(17), do: :local_text_zero
-  def error_code(18), do: :bad_name
-  def error_code(19), do: :index_out_of_range
-  def error_code(20), do: :conference_exists
-  def error_code(21), do: :person_exists
-  def error_code(22), do: :secret_public
-  def error_code(23), do: :letterbox
-  def error_code(24), do: :ldb_error
-  def error_code(25), do: :illegal_misc
-  def error_code(26), do: :illegal_info_type
-  def error_code(27), do: :already_recipient
-  def error_code(28), do: :already_comment
-  def error_code(29), do: :already_footnote
-  def error_code(30), do: :not_recipient
-  def error_code(31), do: :not_comment
-  def error_code(32), do: :not_footnote
-  def error_code(33), do: :recipient_limit
-  def error_code(34), do: :comment_limit
-  def error_code(35), do: :footnote_limit
-  def error_code(36), do: :mark_limit
-  def error_code(37), do: :not_author
-  def error_code(38), do: :no_connect
-  def error_code(39), do: :out_of_memory
-  def error_code(40), do: :server_is_crazy
-  def error_code(41), do: :client_is_crazy
-  def error_code(42), do: :undefined_session
-  def error_code(43), do: :regexp_error
-  def error_code(44), do: :not_marked
-  def error_code(45), do: :temporary_failure
-  def error_code(46), do: :long_array
-  def error_code(47), do: :anonymous_rejected
-  def error_code(48), do: :illegal_aux_item
-  def error_code(49), do: :aux_item_permission
-  def error_code(50), do: :unknown_async
-  def error_code(51), do: :internal_error
-  def error_code(52), do: :feature_disabled
-  def error_code(53), do: :message_not_sent
-  def error_code(54), do: :invalid_membership_type
-  def error_code(55), do: :invalid_range
-  def error_code(56), do: :invalid_range_list
-  def error_code(57), do: :undefined_measurement
-  def error_code(58), do: :priority_denied
-  def error_code(59), do: :weight_denied
-  def error_code(60), do: :weight_zero
-  def error_code(61), do: :bad_bool
+  @errors %{
+    0 => :no_error,
+    2 => :not_implemented,
+    3 => :obsolete_call,
+    4 => :invalid_password,
+    5 => :string_too_long,
+    6 => :login_first,
+    7 => :login_disallowed,
+    8 => :conference_zero,
+    9 => :undefined_conference,
+    10 => :undefined_person,
+    11 => :access_denied,
+    12 => :permission_denied,
+    13 => :not_member,
+    14 => :no_such_text,
+    15 => :text_zero,
+    16 => :no_such_local_text,
+    17 => :local_text_zero,
+    18 => :bad_name,
+    19 => :index_out_of_range,
+    20 => :conference_exists,
+    21 => :person_exists,
+    22 => :secret_public,
+    23 => :letterbox,
+    24 => :ldb_error,
+    25 => :illegal_misc,
+    26 => :illegal_info_type,
+    27 => :already_recipient,
+    28 => :already_comment,
+    29 => :already_footnote,
+    30 => :not_recipient,
+    31 => :not_comment,
+    32 => :not_footnote,
+    33 => :recipient_limit,
+    34 => :comment_limit,
+    35 => :footnote_limit,
+    36 => :mark_limit,
+    37 => :not_author,
+    38 => :no_connect,
+    39 => :out_of_memory,
+    40 => :server_is_crazy,
+    41 => :client_is_crazy,
+    42 => :undefined_session,
+    43 => :regexp_error,
+    44 => :not_marked,
+    45 => :temporary_failure,
+    46 => :long_array,
+    47 => :anonymous_rejected,
+    48 => :illegal_aux_item,
+    49 => :aux_item_permission,
+    50 => :unknown_async,
+    51 => :internal_error,
+    52 => :feature_disabled,
+    53 => :message_not_sent,
+    54 => :invalid_membership_type,
+    55 => :invalid_range,
+    56 => :invalid_range_list,
+    57 => :undefined_measurement,
+    58 => :priority_denied,
+    59 => :weight_denied,
+    60 => :weight_zero,
+    61 => :bad_bool
+  }
 
-  def error_code('0'), do: :no_error
-  def error_code('2'), do: :not_implemented
-  def error_code('3'), do: :obsolete_call
-  def error_code('4'), do: :invalid_password
-  def error_code('5'), do: :string_too_long
-  def error_code('6'), do: :login_first
-  def error_code('7'), do: :login_disallowed
-  def error_code('8'), do: :conference_zero
-  def error_code('9'), do: :undefined_conference
-  def error_code('10'), do: :undefined_person
-  def error_code('11'), do: :access_denied
-  def error_code('12'), do: :permission_denied
-  def error_code('13'), do: :not_member
-  def error_code('14'), do: :no_such_text
-  def error_code('15'), do: :text_zero
-  def error_code('16'), do: :no_such_local_text
-  def error_code('17'), do: :local_text_zero
-  def error_code('18'), do: :bad_name
-  def error_code('19'), do: :index_out_of_range
-  def error_code('20'), do: :conference_exists
-  def error_code('21'), do: :person_exists
-  def error_code('22'), do: :secret_public
-  def error_code('23'), do: :letterbox
-  def error_code('24'), do: :ldb_error
-  def error_code('25'), do: :illegal_misc
-  def error_code('26'), do: :illegal_info_type
-  def error_code('27'), do: :already_recipient
-  def error_code('28'), do: :already_comment
-  def error_code('29'), do: :already_footnote
-  def error_code('30'), do: :not_recipient
-  def error_code('31'), do: :not_comment
-  def error_code('32'), do: :not_footnote
-  def error_code('33'), do: :recipient_limit
-  def error_code('34'), do: :comment_limit
-  def error_code('35'), do: :footnote_limit
-  def error_code('36'), do: :mark_limit
-  def error_code('37'), do: :not_author
-  def error_code('38'), do: :no_connect
-  def error_code('39'), do: :out_of_memory
-  def error_code('40'), do: :server_is_crazy
-  def error_code('41'), do: :client_is_crazy
-  def error_code('42'), do: :undefined_session
-  def error_code('43'), do: :regexp_error
-  def error_code('44'), do: :not_marked
-  def error_code('45'), do: :temporary_failure
-  def error_code('46'), do: :long_array
-  def error_code('47'), do: :anonymous_rejected
-  def error_code('48'), do: :illegal_aux_item
-  def error_code('49'), do: :aux_item_permission
-  def error_code('50'), do: :unknown_async
-  def error_code('51'), do: :internal_error
-  def error_code('52'), do: :feature_disabled
-  def error_code('53'), do: :message_not_sent
-  def error_code('54'), do: :invalid_membership_type
-  def error_code('55'), do: :invalid_range
-  def error_code('56'), do: :invalid_range_list
-  def error_code('57'), do: :undefined_measurement
-  def error_code('58'), do: :priority_denied
-  def error_code('59'), do: :weight_denied
-  def error_code('60'), do: :weight_zero
-  def error_code('61'), do: :bad_bool
+  def async(l) when is_list(l), do: async(List.to_integer(l))
+  def async(s) when is_binary(s), do: async(String.to_integer(s))
+
+  for {number, atom} <- @asyncs do
+    def async(unquote(number)) when is_integer(unquote(number)), do: unquote(atom)
+    def async(unquote(atom)) when is_atom(unquote(atom)), do: unquote(number)
+  end
+
+  def async(_), do: throw("Not a defined asynchronous call")
+
+  def error_code(l) when is_list(l), do: error_code(List.to_integer(l))
+  def error_code(s) when is_binary(s), do: error_code(String.to_integer(s))
+
+  for {number, atom} <- @errors do
+    def error_code(unquote(number)) when is_integer(unquote(number)), do: unquote(atom)
+    def error_code(unquote(atom)) when is_atom(unquote(atom)), do: unquote(number)
+  end
+
+  def error_code(_), do: throw("Not a defined error_codehronous call")
 end
